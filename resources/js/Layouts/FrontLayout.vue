9247f624b0ac014937e08c2d8be2a4c357e87119
WesWeCan <dev@wesleyhartogs.nl>
2023-05-26 14:35:41 +0200

    big bang

---
+++ resources/js/Layouts/FrontLayout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+
+
+
+
+</script>
+
+
+
+<template>
+
+
+<slot></slot>
+
+
+
+</template>
---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+
+import FrontLayout from '@/Layouts/FrontLayout.vue';
+
+</script>
+
+
+<template>
+    <FrontLayout>
+        Hallo!
+    </FrontLayout>
+
+</template>
