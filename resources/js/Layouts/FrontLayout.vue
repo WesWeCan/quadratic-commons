0df0950e389206337865647eaf07dfbca33a523c
WesWeCan <79587028+WesWeCan@users.noreply.github.com>
2024-04-25 12:07:10 +0200

    Added Source Link

--- resources/js/Layouts/FrontLayout.vue
+++ resources/js/Layouts/FrontLayout.vue
@@ -20,6 +20,7 @@
     <span>
         <p>Design: <a href="https://caradt.nl/researcher/tara-karpinski/">Tara Karpinski</a></p>
         <p>Programming: <a href="https://creativetechnologist.nl">Wesley Hartogs</a></p>
+        <p>Source: <a href="https://github.com/WesWeCan/quadratic-commons">GitHub</a></p>
     </span>
 </footer>
 
